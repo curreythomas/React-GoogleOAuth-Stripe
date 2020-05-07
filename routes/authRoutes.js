d319049a42d0545f0b250e2e27758d68cbc1785b
Currey Thomas <currey.thomas@gmail.com>
2020-05-06 23:59:44 -0400

    added readme

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -12,7 +12,7 @@ module.exports = (app) => {
 
   app.get('/api/logout', (req, res) => {
     req.logout();
-    res.send(req.user);
+    res.send('Logged out!');
   });
 
   app.get('/api/current_user', (req, res) => {
